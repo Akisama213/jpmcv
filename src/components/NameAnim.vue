5428eecf1bc56ff082f8e97a493b50b55fecef4d
Jason Piedra <42704934+Akisama213@users.noreply.github.com>
2023-06-02 10:32:29 -0600

    Finished NameAnim

--- src/components/NameAnim.vue
+++ src/components/NameAnim.vue
@@ -1,45 +1,52 @@
 <template>
-  <span
-    v-for="n in SplitLabel.length"
-    :id="label + '.' + (n - 1)"
-    :key="SplitLabel[n - 1]"
-  >
-    {{ SplitLabel[n - 1] }}
+  <space :id="label + '.space'">-</space>
+  <span class="visible">
+    <span
+      v-for="n in SplitLabel.length"
+      :id="label + '.' + (n - 1)"
+      :key="SplitLabel[n - 1]"
+    >
+      {{ SplitLabel[n - 1] }}
+    </span>
   </span>
 </template>
 
 <style>
-template {
-  width: fit-content;
-}
 span {
   opacity: 0;
+  display: inline-block;
+}
+space {
+  width: 0;
+  opacity: 0;
+  transform: translateX(50px);
+  transition: 0.5s;
+  display: inline-block;
+}
+.visible {
+  opacity: 1;
+}
+.spaced {
+  width: auto;
+  transform: translateX(0px);
+  transition: width 0.5s;
 }
 .minus {
+  width: 0;
   opacity: 0;
-  transition: all 0.3s ease-out;
   transform: translateX(-50px);
-  display: inline-block;
-  width: 0%;
-}
-.mayus {
-  width: initial;
-  opacity: 1;
-}
-.space {
-}
-.space::before {
-  content: " ";
+  transition: transform 0.4s ease, opacity 0.1s ease-out;
 }
 .fade {
-  transition: all 0.3s ease-out;
-  width: initial;
   opacity: 1;
   transform: translateX(0);
+  transition: transform 0.3s ease, opacity 0.2s ease-out;
 }
 </style>
 
 <script>
+// transition: transform 0.4s ease, opacity 0.15s;
+// transition: transform 0.3s ease, opacity 0.1s;
 export default {
   name: "NameAnim",
   props: {
@@ -50,67 +57,132 @@ export default {
     return {
       SplitLabel: this.label.split(""),
       wait: Date.now(),
+      extended: false,
     };
   },
   watch: {
+    //Watch function to activate on extend variable change
     extend(val) {
-      console.log("Pre extend", val);
       if (val) {
-        setTimeout(this.animateExtension, this.wait - Date.now());
-      } else {
-        this.wait = Date.now() + 200;
+        //Timeout to have the shortening and extending not overlap
         setTimeout(() => {
-          if (!this.extend) {
-            console.log(this.extend);
+          if (!this.extended) {
+            this.animateExtension();
+            this.extended = true;
+          }
+        }, this.wait - Date.now());
+      } else {
+        //Timeout to have the shortening and extending not overlap
+        this.wait = Date.now() + 300;
+        setTimeout(() => {
+          if (!this.extend & this.extended) {
             this.animateShortening();
-          } else {
-            this.wait = Date.now() + 500;
+            this.extended = false;
           }
         }, this.wait - Date.now());
       }
     },
   },
   methods: {
+    //Function to add the class and function that animate the extension of the characters
     animateExtension() {
-      let timer = setInterval(onTick, 180 / this.label.length);
-      let n = this.label.length - 1;
       let label = this.label;
-      const space = document.getElementById(label + "." + 0);
-      space.classList.add("space");
+      let length = label.length;
+      let timer = setInterval(onTick, 180 / length);
+      let n = length - 1;
+
+      //Animating the space behind the word
+      const span = document.getElementById(label + ".space");
+      span.classList.add("spaced");
+      let show_timer = setInterval(show_onTick, 20 / length);
+      let show_n = 0;
+      function show_onTick() {
+        show_n += 2;
+        if (show_n < span.scrollWidth) {
+          span.style.width = show_n + "px";
+        } else {
+          clearInterval(show_timer);
+          show_timer = null;
+        }
+      }
+
       function onTick() {
         if (n > 0) {
           const span = document.getElementById(label + "." + n);
           span.classList.add("fade");
           n--;
-          //console.log(span);
+
+          //Expanding the width of the span elements one by one manually
+          let show_timer = setInterval(show_onTick, 20 / length);
+          let show_n = 0;
+          function show_onTick() {
+            show_n += 2;
+            if (show_n < span.scrollWidth) {
+              span.style.width = show_n + 1 + "px";
+            } else {
+              clearInterval(show_timer);
+              show_timer = null;
+            }
+          }
         } else {
           clearInterval(timer);
           timer = null;
         }
       }
     },
+    //Function to add the class and function that animate the shortening of the characters
     animateShortening() {
-      let timer = setInterval(onTick, 300 / this.label.length);
-      let n = 0;
       let label = this.label;
-      const space = document.getElementById(label + "." + 0);
-      space.classList.remove("space");
+      let length = label.length;
+      let timer = setInterval(onTick, 300 / length);
+      let n = length - 1;
+
+      //Animating the space behind the word
+      const span = document.getElementById(label + ".space");
+      span.classList.remove("spaced");
+      let hide_n = span.scrollWidth;
+      let hide_timer = setInterval(hide_onTick, 20 / length);
+      function hide_onTick() {
+        hide_n -= 2;
+        if (hide_n > 0) {
+          span.style.width = hide_n - 1 + "px";
+        } else {
+          clearInterval(hide_timer);
+          hide_timer = null;
+        }
+      }
+
+      this.wait = Date.now() + 300;
       function onTick() {
-        if (n < label.length) {
+        if (n > 0) {
           const span = document.getElementById(label + "." + n);
           span.classList.remove("fade");
-          n++;
+          n--;
+
+          //Shortening the width of the span elements one by one manually
+          let hide_n = span.scrollWidth;
+          let hide_timer = setInterval(hide_onTick, 20 / length);
+          function hide_onTick() {
+            hide_n -= 2;
+            if (hide_n > 0) {
+              span.style.width = hide_n - 1 + "px";
+            } else {
+              clearInterval(hide_timer);
+              hide_timer = null;
+            }
+          }
         } else {
           clearInterval(timer);
           timer = null;
         }
       }
-      this.wait = Date.now() + 600;
+      this.wait = Date.now() + 300;
     },
   },
   mounted() {
+    //Initial asignation of classes
     const span = document.getElementById(this.label + "." + 0);
-    span.classList.add("mayus");
+    span.classList.add("visible");
     for (let n = 1; n < this.label.length; n++) {
       const span = document.getElementById(this.label + "." + n);
       span.classList.add("minus");
--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -11,7 +11,47 @@
     </div>
     <!---->
     <div>
-      <text v-for="n in 300" :key="n">Hello, this is "nothing" text. </text>
+      <text
+        >Frontend Developer | Software Developer 1 E-mail jason1234pm@gmail.com
+        Phone (+506) 6071 5699 Address Costa Rica, San José, Moravia, La
+        Trinidad Summary - Dependable Frontend Developer with creative and
+        effective problem solving. - Native Spanish speaker with excellent
+        proficiency in English (C1). - Invested in hard work and collaboration
+        to get accurate solutions efficiently. - Always willing to learn, be it
+        from coworkers or self-studying. - Detail-oriented professional focused
+        in creating intuitive and innovative UIs. Education Liceo Experimental
+        Bilingüe 2014 - 2019 High School Diploma - Conversational English
+        Diploma Tecnológico de Costa Rica 2020 - 2024 Bachelor’s Degree in
+        Computer Engineering Professional Experience Software Engineer – Intel
+        Corporation March 2022 - February 2023 (1 year) Technologies JavaScript,
+        C++, CSS, HTML, .NET, React, MySQL , SSMS, ServiceNow, GitHub, GitLab,
+        Docker, Postman , Jira Accomplishments - Implemented multiple data
+        sorting modules to chart and enlist information for the private online
+        tool my team worked on. - Fixed the technical issues of hundreds of
+        users from 7 countries working on extensive datasets and multiple
+        databases. - Attended daily meetings utilizing Agile method “Scrum”
+        through Jira to administrate challenging deadlines. - Frontend Developer
+        - Ópticas Jimenez March 2023 - Present (Ongoing) Technologies Windows,
+        Virtual Box, Linux, Python, Odoo, Postgres, MySQL, Postgres, HTML, CSS,
+        WordPress, Elementor Accomplishments - Designed the landing page needed
+        for marketing and enhanced the look and feel of all the sections in the
+        company’s site with multiple devices in mind. - Rehabilitated the
+        connection of the main private tool for storing and viewing web orders.
+        - Proposed multiple improvements in the work environment for
+        futureproofing in case more developers come to the company. Jason Piedra
+        Matamoros Frontend Developer | Software Developer 2 E-mail
+        jason1234pm@gmail.com Phone (+506) 6071 5699 Address Costa Rica, San
+        José, Moravia, La Trinidad Technical Skills Programming Languages HTML
+        ●●●●● Python ●●●●● JavaScript - .Net ●●●●● C ●●●●○ C++ ●●●●○ CSS ●●●●○
+        Frontend Vue - Vuetify - Quasar ●●●●● Wordpress - Elementor ●●●●● React
+        ●●●●○ Databases SQL - MySQL ●●●●● MongoDB ●●●●○ Firebase ●●●●○ Engines
+        Unity ●●●●○ Unreal Engine 5 ●●●●○ Godot ●●○○○ Tools Visual Studio Code
+        ●●●●● GitHub ●●●●● Office 365 & Figma ●●●●● Postman ●●●●○ SSMS ●●●●○
+        Docker ●●●●○ Personality Traits Soft Skills Teamwork Active listening
+        Confidence Creativity Patience Attention to detail Assertive
+        communication Adaptability Proactivity Hobbies Exercise Art Gaming Music
+        Series Hangouts</text
+      >
     </div>
   </q-page>
 </template>
